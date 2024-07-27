91d382d6482460bfa0b2a4eb05a63c32d73c4fe3
Shubham Maurya <160427804+shubham-maurya-sm@users.noreply.github.com>
2024-07-28 02:02:12 +0530

    Added new date api.js

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -2,6 +2,16 @@ import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
     const [data,setData]=useState({})
+
+    const getFormattedDate = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}.${month}.${day}`;
+  };
+
+    
   useEffect(() => {
     fetch(
       `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024.7.27/v1/currencies/${currency}.json`)
